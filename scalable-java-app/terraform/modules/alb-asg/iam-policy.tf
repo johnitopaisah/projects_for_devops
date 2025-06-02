23cb3846aa6fff4a452f2cb69840903bd9652ba2
johnitopaisah <johnitopaisah@gmail.com>
2025-06-02 21:07:01 +0200

    Add IAM role and full-access policy for EC2 instances

---
+++ scalable-java-app/terraform/modules/alb-asg/iam-policy.tf
@@ -0,0 +1,50 @@
+resource "aws_iam_role" "iam_role" {
+  name  = "${var.environment}-${var.application}-iam-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = [
+            "ec2.amazonaws.com",
+            "iam.amazonaws.com"
+          ]
+        }
+      }
+    ]
+  })
+  
+  tags = merge(
+    {
+      Name        = "${var.environment}-${var.application}-iam-role",
+      Environment = var.environment,
+      Application = var.application,
+      Owner       = var.owner,
+      CostCenter  = var.cost_center
+    },
+    var.tags
+  )
+}
+
+resource "aws_iam_policy" "iam_policy" {
+  name = "${var.environment}-${var.application}-iam-policy"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid      = "Stmt1693214608532",
+        Action    = "*",
+        Effect    = "Allow",
+        Resource  = "*",
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "aws_iam_role_policy_attachment" {
+  role       = aws_iam_role.iam_role.name
+  policy_arn = aws_iam_policy.iam_policy.arn
+}
